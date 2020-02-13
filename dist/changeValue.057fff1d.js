b8099fc874c9dce2ec6ee45c6e0639518ceda9e6
Elzbieta Kowalska <kowalska.el@gmail.com>
2020-02-13 15:36:49 +0100

    small visual changes

--- dist/changeValue.057fff1d.js
+++ dist/changeValue.057fff1d.js
@@ -125,30 +125,54 @@ document.querySelector('.up-cols').addEventListener('click', function () {
   if (columns.value < 10) {
     columns.value = parseFloat(columns.value) + 1;
   }
+
+  if (columns.value > 9) {
+    columns.style.color = '#ff5719';
+  }
 });
 document.querySelector('.down-cols').addEventListener('click', function () {
   if (columns.value > 8) {
     columns.value = parseFloat(columns.value) - 1;
+  }
+
+  if (columns.value < 10) {
+    columns.style.color = '#2f55a4';
   }
 });
 document.querySelector('.up-rows').addEventListener('click', function () {
   if (rows.value < 10) {
     rows.value = parseFloat(rows.value) + 1;
   }
+
+  if (rows.value > 9) {
+    rows.style.color = '#ff5719';
+  }
 });
 document.querySelector('.down-rows').addEventListener('click', function () {
   if (rows.value > 8) {
     rows.value = parseFloat(rows.value) - 1;
+  }
+
+  if (rows.value < 10) {
+    rows.style.color = '#2f55a4';
   }
 });
 document.querySelector('.up-bombNums').addEventListener('click', function () {
   if (numBombs.value < 30) {
     numBombs.value = parseFloat(numBombs.value) + 1;
   }
+
+  if (numBombs.value > 29) {
+    numBombs.style.color = '#ff5719';
+  }
 });
 document.querySelector('.down-bombNums').addEventListener('click', function () {
   if (numBombs.value > 10) {
     numBombs.value = parseFloat(numBombs.value) - 1;
+  }
+
+  if (numBombs.value < 30) {
+    numBombs.style.color = '#2f55a4';
   }
 });
 },{}],"../node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
--- src/changeValue.js
+++ src/changeValue.js
@@ -6,11 +6,17 @@ document.querySelector('.up-cols').addEventListener('click', () => {
   if (columns.value < 10) {
     columns.value = parseFloat(columns.value) + 1;
   }
+  if (columns.value > 9) {
+    columns.style.color = '#ff5719';
+  }
 });
 
 document.querySelector('.down-cols').addEventListener('click', () => {
   if (columns.value > 8) {
     columns.value = parseFloat(columns.value) - 1;
+  }
+  if (columns.value < 10) {
+    columns.style.color = '#2f55a4';
   }
 });
 
@@ -18,11 +24,17 @@ document.querySelector('.up-rows').addEventListener('click', () => {
   if (rows.value < 10) {
     rows.value = parseFloat(rows.value) + 1;
   }
+  if (rows.value > 9) {
+    rows.style.color = '#ff5719';
+  }
 });
 
 document.querySelector('.down-rows').addEventListener('click', () => {
   if (rows.value > 8) {
     rows.value = parseFloat(rows.value) - 1;
+  }
+  if (rows.value < 10) {
+    rows.style.color = '#2f55a4';
   }
 });
 
@@ -30,10 +42,16 @@ document.querySelector('.up-bombNums').addEventListener('click', () => {
   if (numBombs.value < 30) {
     numBombs.value = parseFloat(numBombs.value) + 1;
   }
+  if (numBombs.value > 29) {
+    numBombs.style.color = '#ff5719';
+  }
 });
 
 document.querySelector('.down-bombNums').addEventListener('click', () => {
   if (numBombs.value > 10) {
     numBombs.value = parseFloat(numBombs.value) - 1;
+  }
+  if (numBombs.value < 30) {
+    numBombs.style.color = '#2f55a4';
   }
 });
